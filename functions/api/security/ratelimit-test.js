42e93c1234e4c2c6b34affeed964f884c780b034
orlandmanagement-alt <orlandmanagement@gmail.com>
2026-03-08 21:20:21 +0700

    Delete functions directory

--- functions/api/security/ratelimit-test.js
+++
@@ -1,7 +0,0 @@
-import { json, requireAuth } from "../../_lib.js";
-
-export async function onRequestGet({ request, env }){
-  const a = await requireAuth(env, request);
-  if(!a.ok) return a.res;
-  return json(200,"ok",{ ts: Date.now() });
-}
