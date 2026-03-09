777081a4b18c4aa761343b3a2510204a60e2a08e
orlandmanagement-alt <orlandmanagement@gmail.com>
2026-03-10 05:48:10 +0700

    Last Sync: 2026-03-10 05:48 (Mobile)

--- sql/cf_analytics_seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT OR IGNORE INTO system_settings (k,v,is_secret,updated_at) VALUES ('cf_analytics_enabled','0',0,strftime('%s','now'));
-INSERT OR IGNORE INTO system_settings (k,v,is_secret,updated_at) VALUES ('cf_analytics_account_id','',0,strftime('%s','now'));
-INSERT OR IGNORE INTO system_settings (k,v,is_secret,updated_at) VALUES ('cf_analytics_zone_tag','',0,strftime('%s','now'));
-INSERT OR IGNORE INTO system_settings (k,v,is_secret,updated_at) VALUES ('cf_analytics_token','',1,strftime('%s','now'));
